570c174a21a54658fbeebdf0eadb2c57002484ba
Henrique Novaes Gonçalves <henriquehng1@gmail.com>
2025-09-18 23:23:11 -0300

    Esqueleto do controller e model DASHBOARD

---
+++ Application/controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+    namespace User\MyFinance\controllers;
+
+    class DashboardController extends Controller{
+
+        public function __construct() {}
+
+        // Método principal para exibir a página do dashboard
+        public function index(){
+        //Lógica de busca de dados do modelo
+        // Lógica para renderizar a view com os dados
+
+        }
+
+        //!!CRIAR O FORMULARIO DE ADICIONAR GASTO 
+        // Método para processar a adição de um novo gasto (POST)
+        public function addExpense(){
+            // Lógica para pegar os dados do formulário e chamar o modelo
+
+        }
+
+
+
+    }
+?>
---
+++ Application/models/DashboardModel.php
@@ -0,0 +1,28 @@
+<?php
+    namespace User\MyFinance\models;
+
+    class DashboardModel extends BaseModel{
+
+        public function __construct() {
+
+        }
+
+        // Método para pegar todos os gastos de um usuário
+        public function getAllUserExpenses(){
+        // A lógica de busca irá aqui
+
+        }
+        // Método para criar um novo gasto
+        public function createExpense(){
+            // A lógica de validação e criação de gastos irá aqui.
+
+        }
+
+
+
+
+
+
+    }
+
+?>
