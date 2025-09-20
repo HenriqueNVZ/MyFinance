a72574cd2ad7586f8a706b3355ffaafc28f1b730
Henrique Novaes Gonçalves <henriquehng1@gmail.com>
2025-09-20 15:14:10 -0300

    comentarios

--- public/styles/javaScript/modal.js
+++ public/styles/javaScript/modal.js
@@ -1,13 +1,15 @@
+//SELETORES
 const modal = document.querySelector(".modal");
 const btnAddExpense = document.querySelector(".btn-add-gasto");
 const btnCloseModal = document.querySelector(".close");
 
+//Abre o modal
 btnAddExpense.addEventListener("click",(event) => {
-    
     event.preventDefault();
     modal.classList.add("active");
 })
 
+//Fecha o modal
 btnCloseModal.addEventListener("click",(event) =>{
     modal.classList.remove("active");
 })
